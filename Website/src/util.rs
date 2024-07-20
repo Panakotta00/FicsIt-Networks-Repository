4c7393a3c9f81d29dfded346af12e95fb3b5f3b3
Panakotta00 <panakotta00@gmail.com>
2024-07-20 15:52:10 +0200

    fix: try fix deploy workflow

--- Website/src/util.rs
+++ Website/src/util.rs
@@ -9,11 +9,11 @@ pub enum URLOrFile {
 
 pub async fn read_file_or_url(url: &str) -> Option<URLOrFile> {
     Some(if let Ok(url) = Url::parse(url) {
-        let response = reqwest::get(url).await.ok()?;
-        let content = response.text().await.ok()?;
+        let response = reqwest::get(url).await.unwrap();
+        let content = response.text().await.unwrap();
         URLOrFile::URL(content)
     } else {
-        let index = File::open(Path::new(url)).ok()?;
+        let index = File::open(Path::new(url)).unwrap();
         URLOrFile::File(index)
     })
 }
