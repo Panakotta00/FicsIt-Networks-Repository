6616ddf266caae1d38b9f07e0faba2d2233fca27
Panakotta00 <panakotta00@gmail.com>
2024-07-21 00:45:01 +0200

    feat: lot a things

--- Website/tailwind.config.js
+++ Website/tailwind.config.js
@@ -1,10 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: ['./templates/**/*.html'],
+    darkMode: "selector",
     theme: {
         extend: {
             fontFamily: {
                 flow: ['"Flow Rounded"', "sans-serif"]
+            },
+            colors: {
+                "primary-bg": "var(--color-primary-bg)",
+                "secondary-bg": "var(--color-secondary-bg)",
+                "primary-fg": "var(--color-primary-fg)",
+                "secondary-fg": "var(--color-secondary-fg)",
+                "accent1": "var(--color-accent1)",
+                "accent2": "var(--color-accent2)",
             }
         },
     },
